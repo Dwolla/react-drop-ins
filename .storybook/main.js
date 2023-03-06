03cc91cdaa3603004d037f0f60417a4593f3d2ed
Shreya Thapa <sthapa@dwolla.com>
2023-03-06 12:48:35 -0600

    Enable storybook to work with Node >v16

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-addon-mock'
   ],
-  framework: '@storybook/react'
+  core: {
+    builder: 'webpack5',
+  },
+  framework: '@storybook/react',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
 };
