dfe81dce11b8ee83ea63661ac01867e61234120e
Shreya Thapa <sthapa@dwolla.com>
2023-03-21 11:32:39 -0500

    Update repo for npm publish

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,21 +4,17 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import typescript from 'rollup-plugin-ts';
 import external from 'rollup-plugin-peer-deps-external';
-import copy from 'rollup-plugin-copy';
-
-const BUNDLE_DIR = 'bundle';
-const PACKAGE_JSON = 'package.json';
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: `${BUNDLE_DIR}/${pkg.main}`,
+      file: `${pkg.main}`,
       format: 'cjs',
       sourcemap: true
     },
     {
-      file: `${BUNDLE_DIR}/${pkg.module}`,
+      file: `${pkg.module}`,
       format: 'es',
       sourcemap: true
     }
@@ -28,9 +24,6 @@ export default {
     nodeResolve(),
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' }),
-    terser(),
-    copy({
-      targets: [{ src: PACKAGE_JSON, dest: BUNDLE_DIR }]
-    })
+    terser()
   ]
 };
